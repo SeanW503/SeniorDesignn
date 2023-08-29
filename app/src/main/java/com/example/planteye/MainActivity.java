aaceb6a194567e5c27fb9b399282ed68b3819ff0
SeanW503 <theseanwhitley@gmail.com>
2023-08-29 17:51:34 -0500

    First Commit: Test

--- app/src/main/java/com/example/planteye/MainActivity.java
+++ app/src/main/java/com/example/planteye/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+//hi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
