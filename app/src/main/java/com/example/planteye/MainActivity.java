009aeb2c0a0a50dbe7424ac9932483ec94251b83
Anthony Maliszewski <amalisz@siue.edu>
2023-09-01 13:26:28 -0500

    This fixed the bug by commenting out both instances of the navigationcontroller on lines 42-44 and lines 89-94 in the MainActivity.java

--- app/src/main/java/com/example/planteye/MainActivity.java
+++ app/src/main/java/com/example/planteye/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,10 +86,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
+    /*@Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
 }
