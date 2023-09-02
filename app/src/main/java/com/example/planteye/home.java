2f59a65465e1003efafb62d643871e8a2c80429e
W00D <woodmeister726@gmail.com>
2023-09-01 21:44:44 -0500

    added settings activity and take photos activity, also enabled button functionality so they navigate to those activities from home page buttons
    the settings button navigates but wont stay on the activity, idk why.

--- app/src/main/java/com/example/planteye/home.java
+++ app/src/main/java/com/example/planteye/home.java
@@ -1,8 +1,12 @@
 package com.example.planteye;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        Button take_photos = findViewById(R.id.takePhotos);
+        ImageButton photo_set = findViewById(R.id.photoSet);
+        ImageButton settings = findViewById(R.id.settings);
+        take_photos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //goes to take photos activity
+                Intent take_photos = new Intent(home.this, takePhotos.class);
+                startActivity(take_photos);
+
+            }
+        });
+
+        photo_set.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent photo_set = new Intent(home.this, photoSet.class);
+                startActivity(photo_set);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent settings = new Intent(home.this, settings.class);
+                startActivity(settings);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/planteye/takePhotos.java
@@ -0,0 +1,14 @@
+package com.example.planteye;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class takePhotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_photos);
+    }
+}
